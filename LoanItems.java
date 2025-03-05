1872e6880b64b715a3ce25c923dd5ee029073e7e
DanielUgueto <daug0001@stud.kea.dk>
2025-03-05 11:10:14 +0100

    Add files via upload

---
+++ LoanItems.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LoanItems {
+    public static void Main(String[] args) {
+        welcomeMessage();
+
+        public static void welcomeMessage () {
+            System.out.println("Velkommen til biblioteket");
+        }
+
+    }
+
+}
